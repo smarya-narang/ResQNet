2befc704e817bab1fb197aa0308aedb8e6db1012
aayushichhabra <aayushichhabra1010@gmail.com>
2025-12-19 01:43:46 +0530

    Delete src/services/supabaseConfig.js

--- src/services/supabaseConfig.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-// ⚠️ REPLACE WITH YOUR KEYS FROM SUPABASE DASHBOARD
-const SUPABASE_URL = 'https://fxmwdwdkgppvhneqxfvf.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ4bXdkd2RrZ3BwdmhuZXF4ZnZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjU4MDA2MjQsImV4cCI6MjA4MTM3NjYyNH0.fNyFvYKy2oMc7f3iOrQRWIRZkqpvVKGUdymraz8zYDg';
-
-export const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
